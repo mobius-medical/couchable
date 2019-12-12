8276b0c1c58f6e3210c4ec13712662e3139044f1
David Dreher <david.dreher@varian.com>
2019-12-12 11:04:15 +0100

    Initial port to Python3
    Updated dependencies with compatibility statements for Python2/3
    Separated string(unicode) and byte handling
    Removed Python2 specific syntax
    Slight refactoring of function names
    Added option to not chunk gzip compression if supported

--- couchable/__init__.py
+++ couchable/__init__.py
@@ -24,7 +24,7 @@ The public API of couchable consists of:
     - L{packer}: Extends the list of built-in or C types supported.
     - L{registerDocType}, L{CouchableDoc}: For adding new document classes.
     - L{registerAttachmentType}, L{CouchableAttachment}: For adding classes to store as attachments.
-    - L{doGzip}, L{doGunzip}: Helper functions for compressing attachments.
+    - L{gzip_compress}, L{gzip_decompress}: Helper functions for compressing attachments.
     - L{newid}: Helper function to make document IDs readable.
 
 For more information, please see:
@@ -41,10 +41,10 @@ From the README.txt:
 --README.txt--
 """
 
-from core import CouchableDb
-from core import registerDocType, CouchableDoc
-from core import registerAttachmentType, CouchableAttachment
-from core import registerPickleType, registerNoneType, registerUncouchableType
-from core import custom_packer
-from core import doGzip, doGunzip
-from core import newid
+from .core import CouchableDb
+from .core import registerDocType, CouchableDoc
+from .core import registerAttachmentType, CouchableAttachment
+from .core import registerPickleType, registerNoneType, registerUncouchableType
+from .core import custom_packer
+from .core import gzip_compress, gzip_decompress
+from .core import newid
--- couchable/testing/test_couchable.py
+++ couchable/testing/test_couchable.py
@@ -18,15 +18,15 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+# Future compatibility
+from __future__ import division, absolute_import, print_function, unicode_literals
 
 # stdlib
 import collections
 import copy
-import cPickle as pickle
 import datetime
 import doctest
 import gc
-import random
 import re
 import sys
 import time
@@ -40,6 +40,12 @@ from nose.plugins.attrib import attr
 # in-house
 import couchable
 import couchable.core
+
+# Compatibility imports
+if sys.version_info.major < 3:
+    import cPickle as pickle
+else:
+    import pickle
 
 #def dumpcdb(func):
 #    def test_dumpcdb_(self):
@@ -242,7 +248,7 @@ class TestCouchable(unittest.TestCase):
         cdb_list = [couchable.CouchableDb('testing_couchable_' + str(i)) for i in range(n)]
         t1 = time.time()
 
-        print 'first', t1-t0
+        print('first', t1-t0)
 
         for cdb in cdb_list:
             del couchdb.Server(cdb.server_url)[cdb.name]
@@ -254,7 +260,7 @@ class TestCouchable(unittest.TestCase):
         cdb_list = [couchable.CouchableDb('testing_couchable_' + str(i)) for i in range(n)]
         t1 = time.time()
 
-        print 'create', t1-t0
+        print('create', t1-t0)
 
         cdb_list = []
         gc.collect()
@@ -263,13 +269,13 @@ class TestCouchable(unittest.TestCase):
         cdb_list = [couchable.CouchableDb('testing_couchable_' + str(i)) for i in range(n)]
         t1 = time.time()
 
-        print 'exists', t1-t0
+        print('exists', t1-t0)
 
         t0 = time.time()
         cdb_list = [couchable.CouchableDb('testing_couchable_' + str(i), exists=True) for i in range(n)]
         t1 = time.time()
 
-        print 'exists w/ flag', t1-t0
+        print('exists w/ flag', t1-t0)
 
         for cdb in cdb_list:
             del couchdb.Server(cdb.server_url)[cdb.name]
@@ -947,7 +953,7 @@ class TestCouchable(unittest.TestCase):
         c = SimpleDoc(name='CCC', attach=SimpleAttachment(c=1, cc=2), bb=b)
         a = SimpleDoc(name='AAA', attach=SimpleAttachment(a=1, aa=2), bb=b,
                 dt=datetime.datetime.now(),
-                td=datetime.timedelta(seconds=1.5),
+                td=datetime.timedelta(seconds=2.5),
                 regex=re.compile('[a-z]+'),
             )
 
@@ -992,7 +998,7 @@ class TestCouchable(unittest.TestCase):
         c = AftermarketDoc(name='CCC', attach=AftermarketAttachment(c=1, cc=2), bb=b)
         a = AftermarketDoc(name='AAA', attach=AftermarketAttachment(a=1, aa=2), bb=b,
                 dt=datetime.datetime.now(),
-                td=datetime.timedelta(seconds=1.5),
+                td=datetime.timedelta(seconds=2.5),
                 regex=re.compile('[a-z]+'),
             )
 
